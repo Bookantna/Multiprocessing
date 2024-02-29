88fc294d4220631cbecddf8cef1671402b1c8818
unknown <phakamatg@gmail.com>
2024-03-01 04:49:24 +0700

    Add GPU test

--- Runner.bat
+++ Runner.bat
@@ -1,7 +1,7 @@
 @echo off
 echo Start testing Sequential filtering
 
-for %%i in (1) do (
+for %%i in (1 5 25 50 100) do (
     echo Current number of pictures: %%i
     python Sequentialprocessing.py 5 %%i
 )
--- tasso.py
+++
@@ -1,6 +0,0 @@
-from transform_M import *
-
-
-
-city = Image(path='city_1920x1080_by_Pedro_lastra.png')
-print(gaussian_blur(city, 5))
